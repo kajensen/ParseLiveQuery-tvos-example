eacce3063ebb5ce9177206d102af3ee86cff639c
johncodeos <johncodeos@gmail.com>
2018-11-28 16:15:03 +0200

    fix didFinishLaunchingWithOptions

--- ParseLiveQuery tvos example/AppDelegate.swift
+++ ParseLiveQuery tvos example/AppDelegate.swift
@@ -13,17 +13,15 @@ import Parse
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
-    private func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         Parse.initialize(with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
             configuration.applicationId = "myAppId"
             configuration.clientKey = "myClientKey"
-            configuration.server = "http://10.0.0.18:4756/parse/"
+            configuration.server = "http://0.0.0.0:0000/parse/"
         }))
         return true
     }
-
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
